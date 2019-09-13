3b37f80f7a7a2c6076822efa642a103def284732
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2019-09-13 09:53:49 +0200

    Service-registration of some general types used in our EPiServer add-ons.

---
+++ Source/Project/ServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO.Abstractions;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Logging;
+using EPiServer.ServiceLocation;
+
+namespace RegionOrebroLan.EPiServer.Initialization
+{
+	/// <summary>
+	/// Service-registration of some general types used in our EPiServer add-ons.
+	/// </summary>
+	[InitializableModule]
+	public class ServiceRegistration : IConfigurableModule
+	{
+		#region Methods
+
+		public virtual void ConfigureContainer(ServiceConfigurationContext context)
+		{
+			if(context == null)
+				throw new ArgumentNullException(nameof(context));
+
+			context.Services.AddSingleton(AppDomain.CurrentDomain);
+			context.Services.AddSingleton<IFileSystem, FileSystem>();
+			context.Services.AddSingleton(LogManager.LoggerFactory());
+		}
+
+		public virtual void Initialize(InitializationEngine context) { }
+		public virtual void Uninitialize(InitializationEngine context) { }
+
+		#endregion
+	}
+}
