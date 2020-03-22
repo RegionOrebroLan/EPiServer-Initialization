8fc413e37583eaf31ed00ccbd83d8cf4e2fb97de
Hans Kindberg <hans.kindberg@regionorebrolan.se>
2020-03-22 17:51:17 +0100

    Prepare more tests.

---
+++ Source/Samples/Injections/AttributeInjectedServiceWithoutServiceType.cs
@@ -0,0 +1,17 @@
+﻿using RegionOrebroLan.ServiceLocation;
+
+namespace RegionOrebroLan.InjectedDependencies
+{
+	/// <summary>
+	/// This is just for testing that the RegionOrebroLan.EPiServer.Initialization.ServiceScanner can handle the RegionOrebroLan.ServiceLocation.ServiceConfigurationAttribute without a ServiceType.
+	/// </summary>
+	[ServiceConfiguration(InstanceMode = InstanceMode.Singleton)]
+	public class AttributeInjectedServiceWithoutServiceType
+	{
+		#region Properties
+
+		public virtual string Value => "Test";
+
+		#endregion
+	}
+}
